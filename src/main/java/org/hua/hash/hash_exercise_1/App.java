e027b275dcecac4eb1b832fdf1ea571a2af2374b
Giorgos <gnicolaides02@gmail.com>
2023-01-06 19:35:40 +0200

    first commit

---
+++ src/main/java/org/hua/hash/hash_exercise_1/App.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hua.hash.hash_exercise_1;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import org.hua.hash.hash_exercise_1.Dictionary.Entry;
+
+public class App {
+
+	public static void main(String args[]) {
+
+		Dictionary<String, Integer> dict = new OpenAdressingHashTable();
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			while(scanner.hasNext()) {
+                            if (scanner.next().equals("s")){
+                                break;
+                            }
+				String line = scanner.nextLine();
+				StringTokenizer st = new StringTokenizer(line);
+				while(st.hasMoreTokens()) { 
+					String word  = st.nextToken();
+					Integer curFreq = dict.get(word);
+					if (curFreq == null) { 
+						curFreq = 1;
+					} else { 
+						curFreq++;
+					}
+					dict.put(word, curFreq);
+                                        System.out.println(dict.size());
+				}
+			}
+                        
+                        
+
+		}
+		System.out.println("wtf");
+		for(Entry<String, Integer> e: dict) { 
+			System.out.println("Word " + e.getKey() + " appeared " + e.getValue() + " times");
+		}
+		for(Entry<String, Integer> e: dict){
+			System.out.println(e.getKey() + "\t" +e.getValue());
+		}
+
+	}
+
+}
